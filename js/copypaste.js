1148b9d0760d6f7c0a932fd500e52c68a481dc66
Jean NiBee <jbateman0769@gmail.com>
2012-04-11 23:27:39 -0400

    Adding testing / POC code for grid copy and paste function

---
+++ js/copypaste.js
@@ -0,0 +1,76 @@
+Ext.ns('gng');
+
+(function(){
+
+  var fakeData = { 
+    items: [{
+      fname: 'John',
+      lname: 'Bateman',
+      rank: 1
+    },{
+      fname: 'Michelle',
+      lname: 'Keefe',
+      rank: 2
+    },{
+      fname: 'Rylie',
+      lname: 'Keefe-Bateman',
+      rank: 3
+    },{
+      fname: 'Mackenzie',
+      lname: 'Keefe-Bateman',
+      rank: 4 
+    },{
+      fname: 'Ryan',
+      lname: 'Keefe-Bateman',
+      rank: 5
+    }]
+  };
+
+  var copyPasteGrid = Ext.extend( Ext.grid.GridPanel, {
+    initComponent: function () {
+      var config = {};
+      this.buildConfig(config);
+      Ext.apply(this, Ext.apply(this.initialConfig, config));
+      copyPasteGrid.superclass.initComponent.call(this);
+    },
+    buildConfig: function (config) {
+      this.buildStore(config);
+      this.buildSelModel(config);
+      this.buildColumns(config);
+      this.buildFar(config);
+    },
+    buildStore: function (config) {
+      config.store = new Ext.data.JSONStore({
+        storeId: 'fakeDataStore',
+        data: fakeData,
+        root: 'items',
+        idProperty: 'rank',
+        fields: ['fname','lname','rank']
+      });
+    },
+    buildSelModel: function (config) {
+      config.selModel = new Ext.grid.CheckboxSelectionModel();
+    },
+    buildColumns: function (config) {
+      config.columns = [
+        config.selModel,
+        { header: 'First Name', sortable: true, renderer: 'string', dataIndex: 'fname'},
+        { header: 'Last Name', sortable: true, renderer: 'string', dataIndex: 'lname'},
+        { header: 'Rank', sortable: true, renderer: 'string', dataIndex: 'rank'}
+      ];
+    },
+    buildFbar: function (config) {
+      config.fbar = {
+        xtype: 'button',
+        text: 'Done',
+        handler: function () {
+          this.close();
+        },
+        scope: this
+      };
+    }
+  });
+
+  Ext.reg('cnpGrid', copyPasteGrid);
+
+}());
